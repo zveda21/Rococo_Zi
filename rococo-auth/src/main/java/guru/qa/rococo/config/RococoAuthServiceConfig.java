f75cb986fa65f54908f4012339baa34816a85101
zveda21 <zveda.hyan@gmail.com>
2025-03-22 22:57:55 +0400

    add rococo auth

---
+++ rococo-auth/src/main/java/guru/qa/rococo/config/RococoAuthServiceConfig.java
@@ -0,0 +1,141 @@
+package guru.qa.rococo.config;
+
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.source.JWKSource;
+import com.nimbusds.jose.proc.SecurityContext;
+import guru.qa.rococo.config.keys.KeyManager;
+import guru.qa.rococo.service.cors.CorsCustomizer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
+import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
+import org.springframework.security.oauth2.core.oidc.OidcScopes;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.server.authorization.client.InMemoryRegisteredClientRepository;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
+import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
+import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer;
+import org.springframework.security.oauth2.server.authorization.settings.AuthorizationServerSettings;
+import org.springframework.security.oauth2.server.authorization.settings.ClientSettings;
+import org.springframework.security.web.PortMapperImpl;
+import org.springframework.security.web.PortResolverImpl;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
+
+import java.security.NoSuchAlgorithmException;
+import java.util.Map;
+import java.util.UUID;
+
+@Configuration
+public class RococoAuthServiceConfig {
+
+    private final KeyManager keyManager;
+    private final String frontUri;
+    private final String authUri;
+    private final CorsCustomizer corsCustomizer;
+    private final String serverPort;
+    private final String defaultHttpsPort = "443";
+
+    @Autowired
+    public RococoAuthServiceConfig(KeyManager keyManager,
+                             @Value("${rococo-ui.uri}") String frontUri,
+                             @Value("${rococo-auth.uri}") String authUri,
+                             CorsCustomizer corsCustomizer,
+                             @Value("${server.port}") String serverPort) {
+        this.keyManager = keyManager;
+        this.frontUri = frontUri;
+        this.authUri = authUri;
+        this.corsCustomizer = corsCustomizer;
+        this.serverPort = serverPort;
+    }
+
+    @Bean
+    @Order(Ordered.HIGHEST_PRECEDENCE)
+    public SecurityFilterChain authorizationServerSecurityFilterChain(HttpSecurity http) throws Exception {
+        OAuth2AuthorizationServerConfiguration.applyDefaultSecurity(http);
+        http.getConfigurer(OAuth2AuthorizationServerConfigurer.class)
+                .oidc(Customizer.withDefaults());    // Enable OpenID Connect 1.0
+
+        http.exceptionHandling(exceptions -> exceptions.authenticationEntryPoint(new LoginUrlAuthenticationEntryPoint("/login")))
+                .oauth2ResourceServer(rs -> rs.jwt(Customizer.withDefaults()));
+
+        corsCustomizer.corsCustomizer(http);
+        return http.build();
+    }
+
+    @Bean
+    public RegisteredClientRepository registeredClientRepository() {
+        RegisteredClient publicClient = RegisteredClient.withId(UUID.randomUUID().toString())
+                .clientId("client")
+                .clientAuthenticationMethod(ClientAuthenticationMethod.NONE)
+                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+                .redirectUri(frontUri + "/authorized")
+                .scope(OidcScopes.OPENID)
+                .scope(OidcScopes.PROFILE)
+                .clientSettings(ClientSettings.builder()
+                        .requireAuthorizationConsent(true)
+                        .requireProofKey(true)
+                        .build()
+                )
+                .build();
+
+        return new InMemoryRegisteredClientRepository(publicClient);
+    }
+
+    @Bean
+    @Profile({"stage"})
+    public LoginUrlAuthenticationEntryPoint loginUrlAuthenticationEntryPointHttps() {
+        LoginUrlAuthenticationEntryPoint entryPoint = new LoginUrlAuthenticationEntryPoint("/login");
+        PortMapperImpl portMapper = new PortMapperImpl();
+        portMapper.setPortMappings(Map.of(
+                serverPort, defaultHttpsPort,
+                "80", defaultHttpsPort,
+                "8080", "8443"
+        ));
+        PortResolverImpl portResolver = new PortResolverImpl();
+        portResolver.setPortMapper(portMapper);
+        entryPoint.setForceHttps(true);
+        entryPoint.setPortMapper(portMapper);
+        entryPoint.setPortResolver(portResolver);
+        return entryPoint;
+    }
+
+    @Bean
+    @Profile({"local", "docker"})
+    public LoginUrlAuthenticationEntryPoint loginUrlAuthenticationEntryPointHttp() {
+        return new LoginUrlAuthenticationEntryPoint("/login");
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Bean
+    public AuthorizationServerSettings authorizationServerSettings() {
+        return AuthorizationServerSettings.builder()
+                .issuer(authUri)
+                .build();
+    }
+
+    @Bean
+    public JWKSource<SecurityContext> jwkSource() throws NoSuchAlgorithmException {
+        JWKSet set = new JWKSet(keyManager.rsaKey());
+        return (jwkSelector, securityContext) -> jwkSelector.select(set);
+    }
+
+    @Bean
+    public JwtDecoder jwtDecoder(JWKSource<SecurityContext> jwkSource) {
+        return OAuth2AuthorizationServerConfiguration.jwtDecoder(jwkSource);
+    }
+}
---
+++ rococo-auth/src/main/java/guru/qa/rococo/service/RococoUserService.java
@@ -0,0 +1,49 @@
+package guru.qa.rococo.service;
+
+import guru.qa.rococo.data.Authority;
+import guru.qa.rococo.data.AuthorityEntity;
+import guru.qa.rococo.data.UserEntity;
+import guru.qa.rococo.data.repository.UserRepository;
+import jakarta.annotation.Nonnull;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+@Component
+public class RococoUserService {
+
+    private static final Logger LOG = LoggerFactory.getLogger(RococoUserService.class);
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public RococoUserService(UserRepository userRepository,
+                             PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Transactional
+    public @Nonnull
+    String registerUser(@Nonnull String username, @Nonnull String password) {
+        UserEntity userEntity = new UserEntity();
+        userEntity.setEnabled(true);
+        userEntity.setAccountNonExpired(true);
+        userEntity.setCredentialsNonExpired(true);
+        userEntity.setAccountNonLocked(true);
+        userEntity.setUsername(username);
+        userEntity.setPassword(passwordEncoder.encode(password));
+
+        AuthorityEntity readAuthorityEntity = new AuthorityEntity();
+        readAuthorityEntity.setAuthority(Authority.read);
+        AuthorityEntity writeAuthorityEntity = new AuthorityEntity();
+        writeAuthorityEntity.setAuthority(Authority.write);
+
+        userEntity.addAuthorities(readAuthorityEntity, writeAuthorityEntity);
+        return userRepository.save(userEntity).getUsername();
+    }
+}
