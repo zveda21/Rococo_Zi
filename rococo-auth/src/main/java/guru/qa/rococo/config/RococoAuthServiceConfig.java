503307908496ffc9f15fa0164bf425d9bc5dac0a
zveda21 <zveda.hyan@gmail.com>
2025-03-23 17:27:25 +0400

    add some configs

--- rococo-auth/src/main/java/guru/qa/rococo/config/RococoAuthServiceConfig.java
+++ rococo-auth/src/main/java/guru/qa/rococo/config/RococoAuthServiceConfig.java
@@ -5,6 +5,8 @@ import com.nimbusds.jose.jwk.source.JWKSource;
 import com.nimbusds.jose.proc.SecurityContext;
 import guru.qa.rococo.config.keys.KeyManager;
 import guru.qa.rococo.service.cors.CorsCustomizer;
+import guru.qa.rococo.service.SpecificRequestDumperFilter;
+import org.apache.catalina.filters.RequestDumperFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -12,11 +14,14 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
+import org.springframework.core.env.Environment;
+import org.springframework.core.env.Profiles;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.core.AuthorizationGrantType;
+import org.springframework.security.oauth2.server.authorization.settings.TokenSettings;
 import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
 import org.springframework.security.oauth2.core.oidc.OidcScopes;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
@@ -31,8 +36,11 @@ import org.springframework.security.web.PortMapperImpl;
 import org.springframework.security.web.PortResolverImpl;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
+import org.springframework.security.web.session.DisableEncodeUrlFilter;
 
 import java.security.NoSuchAlgorithmException;
+import java.time.Duration;
+import java.time.temporal.ChronoUnit;
 import java.util.Map;
 import java.util.UUID;
 
@@ -40,33 +48,47 @@ import java.util.UUID;
 public class RococoAuthServiceConfig {
 
     private final KeyManager keyManager;
-    private final String frontUri;
-    private final String authUri;
+    private final String rococoFrontUri;
+    private final String rococoAuthUri;
+    private final String clientId;
     private final CorsCustomizer corsCustomizer;
     private final String serverPort;
     private final String defaultHttpsPort = "443";
+    private final Environment environment;
 
     @Autowired
     public RococoAuthServiceConfig(KeyManager keyManager,
-                             @Value("${rococo-front.base-uri}") String frontUri,
-                             @Value("${rococo-auth.base-uri}") String authUri,
-                             CorsCustomizer corsCustomizer,
-                             @Value("${server.port}") String serverPort) {
+                                   @Value("${rococo-front.base-uri}") String rococoFrontUri,
+                                   @Value("${rococo-auth.base-uri}") String rococoAuthUri,
+                                   @Value("${oauth2.client-id}") String clientId,
+                                   @Value("${server.port}") String serverPort,
+                                   CorsCustomizer corsCustomizer,
+                                   Environment environment) {
         this.keyManager = keyManager;
-        this.frontUri = frontUri;
-        this.authUri = authUri;
-        this.corsCustomizer = corsCustomizer;
+        this.rococoFrontUri = rococoFrontUri;
+        this.rococoAuthUri = rococoAuthUri;
+        this.clientId = clientId;
         this.serverPort = serverPort;
+        this.corsCustomizer = corsCustomizer;
+        this.environment = environment;
     }
 
     @Bean
     @Order(Ordered.HIGHEST_PRECEDENCE)
-    public SecurityFilterChain authorizationServerSecurityFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain authorizationServerSecurityFilterChain(HttpSecurity http,
+                                                                      LoginUrlAuthenticationEntryPoint entryPoint) throws Exception {
         OAuth2AuthorizationServerConfiguration.applyDefaultSecurity(http);
+        if (environment.acceptsProfiles(Profiles.of("local", "staging"))) {
+            http.addFilterBefore(new SpecificRequestDumperFilter(
+                    new RequestDumperFilter(),
+                    "/login", "/oauth2/.*"
+            ), DisableEncodeUrlFilter.class);
+        }
+
         http.getConfigurer(OAuth2AuthorizationServerConfigurer.class)
                 .oidc(Customizer.withDefaults());    // Enable OpenID Connect 1.0
 
-        http.exceptionHandling(exceptions -> exceptions.authenticationEntryPoint(new LoginUrlAuthenticationEntryPoint("/login")))
+        http.exceptionHandling(exceptions -> exceptions.authenticationEntryPoint(entryPoint))
                 .oauth2ResourceServer(rs -> rs.jwt(Customizer.withDefaults()));
 
         corsCustomizer.corsCustomizer(http);
@@ -74,26 +96,7 @@ public class RococoAuthServiceConfig {
     }
 
     @Bean
-    public RegisteredClientRepository registeredClientRepository() {
-        RegisteredClient publicClient = RegisteredClient.withId(UUID.randomUUID().toString())
-                .clientId("client")
-                .clientAuthenticationMethod(ClientAuthenticationMethod.NONE)
-                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
-                .redirectUri(frontUri + "/authorized")
-                .scope(OidcScopes.OPENID)
-                .scope(OidcScopes.PROFILE)
-                .clientSettings(ClientSettings.builder()
-                        .requireAuthorizationConsent(true)
-                        .requireProofKey(true)
-                        .build()
-                )
-                .build();
-
-        return new InMemoryRegisteredClientRepository(publicClient);
-    }
-
-    @Bean
-    @Profile({"stage"})
+    @Profile({"staging", "prod"})
     public LoginUrlAuthenticationEntryPoint loginUrlAuthenticationEntryPointHttps() {
         LoginUrlAuthenticationEntryPoint entryPoint = new LoginUrlAuthenticationEntryPoint("/login");
         PortMapperImpl portMapper = new PortMapperImpl();
@@ -117,6 +120,28 @@ public class RococoAuthServiceConfig {
     }
 
     @Bean
+    public RegisteredClientRepository registeredClientRepository() {
+        RegisteredClient publicClient = RegisteredClient.withId(UUID.randomUUID().toString())
+                .clientId(clientId)
+                .clientAuthenticationMethod(ClientAuthenticationMethod.NONE)
+                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+                .redirectUri(rococoFrontUri + "/authorized")
+                .scope(OidcScopes.OPENID)
+                .scope(OidcScopes.PROFILE)
+                .clientSettings(ClientSettings.builder()
+                        .requireAuthorizationConsent(true)
+                        .requireProofKey(true)
+                        .build()
+                )
+                .tokenSettings(TokenSettings.builder()
+                        .accessTokenTimeToLive(Duration.of(2, ChronoUnit.HOURS))
+                        .build())
+                .build();
+
+        return new InMemoryRegisteredClientRepository(publicClient);
+    }
+
+    @Bean
     public PasswordEncoder passwordEncoder() {
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
@@ -124,7 +149,7 @@ public class RococoAuthServiceConfig {
     @Bean
     public AuthorizationServerSettings authorizationServerSettings() {
         return AuthorizationServerSettings.builder()
-                .issuer(authUri)
+                .issuer(rococoAuthUri)
                 .build();
     }
 
--- rococo-auth/src/main/java/guru/qa/rococo/controller/LoginController.java
+++ rococo-auth/src/main/java/guru/qa/rococo/controller/LoginController.java
@@ -1,5 +1,8 @@
 package guru.qa.rococo.controller;
 
+import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.web.savedrequest.DefaultSavedRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -7,9 +10,21 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class LoginController {
 
     private static final String LOGIN_VIEW_NAME = "login";
+    private static final String PRE_REQ_ATTR = "SPRING_SECURITY_SAVED_REQUEST";
+    private static final String PRE_REQ_URI = "/oauth2/authorize";
+
+    private final String rococoFrontUri;
+
+    public LoginController(@Value("${rococo-front.base-uri}") String rococoFrontUri) {
+        this.rococoFrontUri = rococoFrontUri;
+    }
 
     @GetMapping("/login")
-    public String login() {
+    public String login(HttpSession session) {
+        DefaultSavedRequest savedRequest = (DefaultSavedRequest) session.getAttribute(PRE_REQ_ATTR);
+        if (savedRequest == null || !savedRequest.getRequestURI().equals(PRE_REQ_URI)) {
+            return "redirect:" + rococoFrontUri;
+        }
         return LOGIN_VIEW_NAME;
     }
 }
