ba3ee2ad5cc970f0324f8775ffd506804fa5143c
zveda21 <zveda.hyan@gmail.com>
2025-03-24 22:13:58 +0400

    add artist controller

--- rococo-artist/src/main/java/guru/qa/rococo/controller/ArtistController.java
+++ rococo-artist/src/main/java/guru/qa/rococo/controller/ArtistController.java
@@ -1,4 +1,27 @@
 package guru.qa.rococo.controller;
 
+import guru.qa.rococo.model.ArtistJson;
+import guru.qa.rococo.service.ArtistService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/internal/artist")
 public class ArtistController {
+
+    private final ArtistService artistService;
+
+    public ArtistController(ArtistService artistService) {
+        this.artistService = artistService;
+    }
+
+    @GetMapping
+    public Page<ArtistJson> getAllArtists(Pageable pageable, @RequestParam(required = false) String name) {
+        return artistService.getAll(name, pageable);
+    }
+
 }
