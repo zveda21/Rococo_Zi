aff5def4b06d3e618c3ae0f56cfb73bc22dd1de8
zveda21 <zveda.hyan@gmail.com>
2025-03-28 23:20:23 +0400

    script to prepare museum db

--- local-setup.sh
+++ local-setup.sh
@@ -2,13 +2,16 @@
 
 chmod +x postgres/script/init-database.sh
 
-docker compose down
+docker volume rm rococo_zi_pgdata
+docker stop $(docker ps -a -q)
+docker rm $(docker ps -a -q)
 docker compose up -d
 
 gradle build bootJar
 
 echo "################ Setup completed ################"
-echo "# Run three commands in separate terminals"
+echo "# Run each command in separate terminal"
 echo "# ./local-rococo-artist.sh"
+echo "# ./local-rococo-museum.sh"
 echo "# ./local-rococo-gateway.sh"
 echo "# ./local-rococo-userdata.sh"
