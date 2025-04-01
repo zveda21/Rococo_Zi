1cade573a1537e4e29c9df097dadc80b36a1a560
zveda21 <zveda.hyan@gmail.com>
2025-04-01 12:32:01 +0400

    ascii art

--- local-setup.sh
+++ local-setup.sh
@@ -11,3 +11,19 @@ docker compose up -d
 
 # Build all and skip e2e tests until everything is up and running
 ./gradlew build bootJar -x :rococo-e2e-test:test
+
+echo -e "\e[1;32m"
+cat << "EOF"
+ _______   ______   .__   __.  _______
+|       \ /  __  \  |  \ |  | |   ____|
+|  .--.  |  |  |  | |   \|  | |  |__
+|  |  |  |  |  |  | |  . `  | |   __|
+|  '--'  |  `--'  | |  |\   | |  |____
+|_______/ \______/  |__| \__| |_______|
+
+✅ Docker setup completed.
+🚀 Setup and build completed successfully! 🚀
+➡️ Run the 'local-all.sh' script to start all the services.
+
+EOF
+echo -e "\e[0m"
