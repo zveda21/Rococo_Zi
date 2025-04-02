dd2959492a8017b0c80726c0003471e918db7524
zveda21 <zveda.hyan@gmail.com>
2025-04-02 15:15:32 +0400

    fix test

--- rococo-e2e-tests/src/test/java/qa/guru/rococo/test/api/OAuthTest.java
+++ rococo-e2e-tests/src/test/java/qa/guru/rococo/test/api/OAuthTest.java
@@ -2,21 +2,28 @@ package qa.guru.rococo.test.api;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.extension.RegisterExtension;
 import qa.guru.rococo.jupiter.annotation.ApiLogin;
 import qa.guru.rococo.jupiter.annotation.Token;
 import qa.guru.rococo.jupiter.annotation.User;
+import qa.guru.rococo.jupiter.annotation.meta.RestTest;
 import qa.guru.rococo.jupiter.extension.ApiLoginExtension;
 import qa.guru.rococo.jupiter.extension.TestMethodContextExtension;
-import qa.guru.rococo.jupiter.extension.UserExtension;
 import qa.guru.rococo.model.rest.UserJson;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@ExtendWith({ApiLoginExtension.class, TestMethodContextExtension.class, UserExtension.class})
+@RestTest
 public class OAuthTest {
+
+    @RegisterExtension
+    @SuppressWarnings("unused")
+    private static final ApiLoginExtension apiLoginExtension = ApiLoginExtension.rest();
 
     @Test
     @ApiLogin
+    @User
+    @ExtendWith(TestMethodContextExtension.class)
     void shouldGetTokenWithDefaultUser(@Token String token) {
         assertNotNull(token);
     }
